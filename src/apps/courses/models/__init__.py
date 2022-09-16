bf473159b48a607cc93eca863a64fa29734db333
Jefferson472 <jefferson472@hotmail.com>
2022-09-16 07:48:39 -0300

    feat: add BaseModel

--- src/apps/courses/models/__init__.py
+++ src/apps/courses/models/__init__.py
@@ -1,3 +1,5 @@
 from apps.courses.models.Course import Course
 from apps.courses.models.Subject import Subject
 from apps.courses.models.Module import Module
+from apps.courses.models.Content import Content
+from apps.courses.models.ItemBase import Text, File, Image, Video
