578fb3f973bfea8f5725c543a63fd667c5dad38a
Jefferson472 <jefferson472@hotmail.com>
2022-09-17 12:59:41 -0300

    feat: add course list, detail view, url, template

--- src/apps/courses/views/CourseViews.py
+++ src/apps/courses/views/CourseViews.py
@@ -1,9 +1,12 @@
+from django.db.models import Count
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView
+from django.views import generic
+from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-from apps.courses.models import Course
+from apps.courses.models import Course, Subject
 
 
 class OwnerMixin(object):
@@ -27,7 +30,7 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
     template_name = 'courses/manage/course/form.html'
 
 
-class ManageCourseListView(OwnerCourseMixin, ListView):
+class ManageCourseListView(OwnerCourseMixin, generic.ListView):
     template_name = 'courses/manage/course/list.html'
     permission_required = 'courses.view_course'
 
@@ -43,3 +46,26 @@ class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = 'courses/manage/course/delete.html'
     permission_required = 'courses.delete_course'
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses')) # add o retorno do total de cursos conforme a related_name=courses em Course
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({
+            'subjects': subjects,
+            'subject': subject,
+            'courses': courses
+        })
+
+
+class CourseDetailView(generic.DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
