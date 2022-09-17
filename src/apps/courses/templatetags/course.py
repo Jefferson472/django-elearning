2e2646f0f7d65055f0d418c601dd2b95744fc8d3
Jefferson472 <jefferson472@hotmail.com>
2022-09-17 08:06:41 -0300

    feat: add templatetag model_name

--- src/apps/courses/templatetags/course.py
+++ src/apps/courses/templatetags/course.py
@@ -1,1 +1,13 @@
+from inspect import Attribute
+from django import template
+
+
+register = template.Library()
+
+@register.filter
+def model_name(obj):
     """Este é um filtro de template que pode ser aplicado como object|model_name para obter o nome do modelo de um objeto"""
+    try:
+        return obj._meta.model_name
+    except AttributeError:
+        return None
