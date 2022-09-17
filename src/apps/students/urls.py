144493e737befccbe782265f8d1f06aa2885b005
Jefferson472 <jefferson472@hotmail.com>
2022-09-17 15:25:34 -0300

    feat: add view, url subscrption system

--- src/apps/students/urls.py
+++ src/apps/students/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
 from apps.students.views import StudentRegistrationView
+from apps.students.views import StudentEnrollCourseView
 
 
 urlpatterns = [
     path('register/',
         StudentRegistrationView.as_view(), name='student_registration'),
+    
+    path('enroll-course/',
+        StudentEnrollCourseView.as_view(), name='student_course_detail')
 ]
