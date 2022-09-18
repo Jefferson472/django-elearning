43ce93a2eee2f49dfd45bbd58e4db58da1e657bb
Jefferson472 <jefferson472@hotmail.com>
2022-09-18 11:35:44 -0300

    feat: add cache listview courses

--- src/apps/courses/views/CourseViews.py
+++ src/apps/courses/views/CourseViews.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.core.cache import cache
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
@@ -54,7 +55,11 @@ class CourseListView(TemplateResponseMixin, View):
     template_name = 'courses/course/list.html'
 
     def get(self, request, subject=None):
-        subjects = Subject.objects.annotate(total_courses=Count('courses')) # add o retorno do total de cursos conforme a related_name=courses em Course
+        subjects = cache.get('all_subjects') # tenta acessar a chave no cache
+        if not subjects:
+            #add o retorno do total de cursos conforme a related_name=courses em Course
+            subjects = Subject.objects.annotate(total_courses=Count('courses'))
+            cache.set('all_subjects', subjects) # add a chave ao cache
         courses = Course.objects.annotate(total_modules=Count('modules'))
 
         if subject:
