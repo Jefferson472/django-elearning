5af4cb420d027ee3b23a029c55afae315660c902
Jefferson472 <jefferson472@hotmail.com>
2022-09-18 16:55:00 -0300

    feat: add viewset subscription

--- src/apps/courses/api/urls.py
+++ src/apps/courses/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from apps.courses.api.views import (
-    SubjectListView, SubjectDetailView, CourseEnrollView, CourseViewSet
+    SubjectListView, SubjectDetailView, CourseViewSet #, CourseEnrollView
 )
 
 
@@ -14,6 +14,6 @@ app_name = 'courses'
 urlpatterns = [
     path('subjects/', SubjectListView.as_view(), name='subject_list'),
     path('subjects/<pk>/', SubjectDetailView.as_view(), name='subject_detail'),
-    path('courses/<pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll'),
+    # path('courses/<pk>/enroll/', CourseEnrollView.as_view(), name='course_enroll'), # agora essa url é gerada pelo viewset eroute 
     path('', include(router.urls)),
 ]
--- src/apps/courses/api/views.py
+++ src/apps/courses/api/views.py
@@ -1,7 +1,9 @@
+from xmlrpc.client import ResponseError
 from django.shortcuts import get_object_or_404
 
 from rest_framework import generics, viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -21,16 +23,29 @@ class SubjectDetailView(generics.RetrieveAPIView):
     serializer_class = SubjectSerializer
 
 
-class CourseEnrollView(APIView):
-    authentication_classes = (BasicAuthentication,)
-    permission_classes = (IsAuthenticated,)
+# class CourseEnrollView(APIView):
+    """Essa class foi commitada para exemplicar que a CourseViewSet faz a mesma coisa"""
+#     authentication_classes = (BasicAuthentication,)
+#     permission_classes = (IsAuthenticated,)
 
-    def post(self, request, pk, format=None):
-        course = get_object_or_404(Course, pk=pk)
-        course.students.add(request.user)
-        return Response({'enrolled': True})
+#     def post(self, request, pk, format=None):
+#         course = get_object_or_404(Course, pk=pk)
+#         course.students.add(request.user)
+#         return Response({'enrolled': True})
 
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    """Essa ViewSet irá gerar automaticamente a url para inscrição no curso"""
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(
+        detail=True,
+        methods=['post'],
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated],
+    )
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
