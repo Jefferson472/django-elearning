293888bb95a3c0f7795a55ec8b68c4ca5596afab
Jefferson472 <jefferson472@hotmail.com>
2022-09-18 16:11:32 -0300

    feat: serializadores aninhados

--- src/apps/courses/api/serializers.py
+++ src/apps/courses/api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 
+from apps.courses.models.Course import Course
+from apps.courses.models.Module import Module
 from apps.courses.models.Subject import Subject
 
 
@@ -7,3 +9,20 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id', 'title', 'slug']
+
+
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    modules = ModuleSerializer(many=True, read_only=True) # serializando vários objetos
+
+    class Meta:
+        model = Course
+        fields = [
+            'id', 'subject', 'title', 'slug', 'overview'
+            'created', 'owner', 'modules'
+        ]
