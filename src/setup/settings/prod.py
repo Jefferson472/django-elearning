5d92add81794eb70e66d1f96638419e3c1a12a46
Jefferson472 <jefferson472@hotmail.com>
2022-09-19 17:31:17 -0300

    build: install, config postgresql docker compose

--- src/setup/settings/prod.py
+++ src/setup/settings/prod.py
@@ -10,6 +10,11 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     "default": {
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
     }
 }
