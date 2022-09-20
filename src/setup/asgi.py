b44dc379278081538ec117438462e7dbadfc0055
Jefferson472 <jefferson472@hotmail.com>
2022-09-20 18:28:00 -0300

    feat: add config to daphne

--- src/setup/asgi.py
+++ src/setup/asgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 
 from channels.auth import AuthMiddlewareStack # identifica o usuário que está enviando a mensagem
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.security.websocket import AllowedHostsOriginValidator
 
 from  apps.chat.routing import websocket_urlpatterns
@@ -21,6 +21,10 @@ from  apps.chat.routing import websocket_urlpatterns
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "setup.settings")
 
 django_asgi_app = get_asgi_application()
+
+# configurando para o channels para utilizar o socket daphne
+# django.setup()
+# django_asgi_app = get_default_application()
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
